01e21b312fd29f8636e94d5fd0be031c10f30c88
Hean <jhubert@heanlab.com>
2025-04-09 12:06:53 +0200

    [FIX]: Add mysql reconnect

--- app.py
+++ app.py
@@ -13,19 +13,26 @@ app = Flask(__name__)
 CORS(app)
 app.config.from_object(Config)
 
+#MysQL configuration
+mysql_config = {
+    'host': os.getenv('MYSQL_HOST'),
+    'port': app.config["MYSQL_PORT"],
+    'user': app.config["DATABASE_USER"],
+    'password': app.config["DATABASE_PWD"],
+    'database': app.config["DATABASE"],
+    'raise_on_warnings': True
+}
 
 #Connect to server
-cnx = mysql.connector.connect(
-            host=os.getenv('MYSQL_HOST'),
-            port=app.config["MYSQL_PORT"],
-            user=app.config["DATABASE_USER"],
-            password=app.config["DATABASE_PWD"],
-            database=app.config["DATABASE"]
-        )
+cnx = mysql.connector.connect(**mysql_config)
 
 @app.route("/getprojects")
 def getprojects():
-    cur = cnx.cursor(dictionary=True)
+    if  cnx.is_connected():
+        cur = cnx.cursor(dictionary=True, buffered=True)
+    else:
+        cnx.reconnect()
+        cur = cnx.cursor(dictionary=True, buffered=True)
     try:
         query=("SELECT * from heanlab.project ;")
         cur.execute(query)
@@ -33,17 +40,18 @@ def getprojects():
     finally:
         cur.close()
 
-    return []
 
 
 @app.route('/getabout')
 def getabout():
-    cur = cnx.cursor(dictionary = True)
+    if cnx.is_connected():
+        cur = cnx.cursor(dictionary = True, buffered = True)
+    else:
+        cnx.reconnect()
+        cur = cnx.cursor(dictionary = True, buffered = True)
     try :
         query =  f"SELECT * FROM {app.config['DATABASE']}.about;"
         cur.execute(query)
         return jsonify(cur.fetchone())
     finally:
         cur.close()
-
-    pass
